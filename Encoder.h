5a8d79a8e449fe6e39ddc2b06ab91bdc3ebc9e10
Petr Kratochvíl <56606404+cratas@users.noreply.github.com>
2020-12-01 17:58:09 +0100

    Update Encoder.h

--- Encoder.h
+++ Encoder.h
@@ -1,23 +1,38 @@
 #pragma once
+#include "Node.h"
 #include <iostream>
 #include <map>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <functional>
+#include <set>
+
 using namespace std;
 
 class Encoder
 {
 private:
-	string filename;
+	string inputFile;
+	string outputFile;
 	string inputText;
 	map<char, int> frequencies;
-	
+	vector<pair<char, int>> sortedFrequencies;
+	priority_queue<Node*, vector<Node*>, comp> priorityQueue;
+
 	void loadText();
-	
+	void encode(Node* root, string str, unordered_map<char, string>& huffmanCode);
 
 public:
-	Encoder(string filename);
+	Encoder(string filename, string outputFile);
 	string getInputText();
 	void setFrequencies();
 	void printFrequencies();
-	void sortFrequencies();
+	void setSortedFrequencies();
+	Node* getNode(char ch, int freq, Node* left, Node* right);
+	void buildHuffmanTree();
 };
 
